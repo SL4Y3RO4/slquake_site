644890c671d9a238d837e83fdf3bb15253d01617
SL4Y3RO4 <slayr2335@gmail.com>
2026-04-26 19:12:18 +0200

    all

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let searcher = document.getElementById("searcher");
+let every_paragh = document.querySelectorAll("p");
+
+let i = 0;
+let string = [];
+
+for(let i = 0; i < searcher.length; i++) {
+    string.push(searcher[i].value);
+}
+
+
+for(let i = 0; i < string.length; i++) {
+
+    if(every_paragh[i].innerText.equals(string[i])) {
+        every_paragh[i].style.color = "red";
+    }
+}
+*/
